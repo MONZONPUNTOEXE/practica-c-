5356ecb21ea7682001b809f37c2a8cdda77ecfbb
franco <franqoto@gmail.com>
2024-05-14 16:57:53 -0300

    finish 2nd class

--- clase2-math-cpp/main.cpp
+++ clase2-math-cpp/main.cpp
@@ -1,13 +1,61 @@
 #include <iostream>
+
+// importamos la libreria math.h
 #include <math.h>
 
 using namespace std;
 
 int main()
-{
-    int a = 10;
-    int b = 10;
+{   // declaramos las variables
+    float a = 20;
+    float b = 50;
 
-    cout << a + b;
+    // for division and remainder
+    int c = a;
+    int d = b;
+
+    cout << "a vale : " << a << endl;
+    cout << "b vale : " << b << endl;
+    cout << "\n";
+
+    // suma
+    cout << "Resultado de a + b: " << a + b << endl;
+    // resta
+    cout << "Resultado de a - b: " << a - b << endl;
+    // Division
+    cout << "Resultado de a / b: " << a / b << endl;
+    // Multiplicacion
+    cout << "Resultado de a * b: " << a * b << endl;
+    // Restos
+    cout << "Resultado de a % b: " << c % d << endl;
+
+    // WE WRITE IT DYNAMICALLY
+
+    int s = a + b;
+    int subs = a - b;
+    int mul = a * b;
+    float div = a / b;
+    int remainder = c % d;
+
+        // IN THIS WAY WE WILL OBTAIN THE RESULT OF THE SUM
+
+    cout << "La SUMA entre " << a << " y " << b << " es " << s << endl;
+
+        // IN THIE WAY WILL WE OBTAIN THE RESULT OF THE SUBSTRACTION
+
+    cout << "La RESTA entre " << a << " y " << b << " es " << subs << endl;
+
+        // IN THIS WAY WE WILL OBTAIL THE RESULT OF THIS MULTIPLICATION
+
+    cout << "La MULTIPLICACION entre " << a << " y " << b << " es " << mul << endl;
+
+        // IN THIS WAY WE WILL OBTAIN THE RESULT OF THIS DIVISION
+
+    cout << "La DIVISION entre " << a << " y " << b << " es " << div << endl;
+
+            // IN THIS WAY WE WILL OBTAIN THE RESULT OF THIS REMAINDER
+
+    cout << "El RESTO entre " << a << " y " << b << " es " << remainder << endl;
+
     return 0;
 }
