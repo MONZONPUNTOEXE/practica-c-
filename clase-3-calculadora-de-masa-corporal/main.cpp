61e913dcc831ec172223cd46ccdee28466958d30
Franco Monzon <monzonfranco95@gmail.com>
2024-05-15 18:12:33 -0300

    add imc calculator

--- clase-3-calculadora-de-masa-corporal/main.cpp
+++ clase-3-calculadora-de-masa-corporal/main.cpp
@@ -5,6 +5,8 @@ el indice de masa corporal o IMC
 
 // We adding library for out and in data
 #include <iostream>
+#include <math.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -14,6 +16,24 @@ int main(){
     float height = 0;
     float result = 0;
 
+    //  I solved it this way
+
+    /*
+    // weight data
+    cout << "Indice de Masa Muscular Corporal (IMC)\n";
+    cout << "Ingrese su peso en kilogramos: ";
+    cin >> weight;
+
+    // height data
+    cout << "Ingrese su altura en metros: ";
+    cin >> height;
+
+    // calculate
+
+    result = weight / (height * height);
+    */
+
+    // The course used the math library and iomanip for decimal precision
 
     // weight data
     cout << "Indice de Masa Muscular Corporal (IMC)\n";
@@ -26,7 +46,9 @@ int main(){
 
     // calculate
 
-    cout << "Su IMC es de: " << result =  weight / height * 2 << endl;
+    result = weight / pow(height,2);
+
+    cout << "Su IMC es de: " << setprecision(4) << result << " kg/m^2" << endl;
 
 
     return 0;
